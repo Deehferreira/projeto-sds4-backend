4b50f6f8bab1c450f8d137c6fdc1c045c838b24a
Debora Ferreira <deborayferreirah@gmail.com>
2021-09-12 01:30:55 -0300

    first commit

---
+++ beckend/src/main/java/com/devsuperior/dsvendas/controllers/SaleControllers.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dsvendas.controllers;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleControllers {
+
+    @Autowired
+    private SaleService service;
+
+    @GetMapping
+    public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable){
+        Page<SaleDTO> page = service.findAll(pageable);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping(value = "/amount-by-saller")
+    public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller(){
+        List<SaleSumDTO> list = service.amountGroupedBySeller();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/success-by-saller")
+    public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller(){
+        List<SaleSuccessDTO> list = service.successGroupedBySeller();
+        return ResponseEntity.ok(list);
+    }
+
+
+
+}
---
+++ beckend/src/main/java/com/devsuperior/dsvendas/controllers/SellerControllers.java
@@ -0,0 +1,33 @@
+package com.devsuperior.dsvendas.controllers;
+
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SellerDTO;
+import com.devsuperior.dsvendas.service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/sellers")
+public class SellerControllers {
+
+    @Autowired
+    private SellerService service;
+
+    @GetMapping
+    public ResponseEntity<List<SellerDTO>> findAll(){
+        List<SellerDTO> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/success-by-saller")
+    public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller(){
+        List<SaleSuccessDTO> list = service.successGroupedBySeller();
+        return ResponseEntity.ok(list);
+    }
+
+}
